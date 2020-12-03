2232ce4f04ca8c7ff327098bf57b47c61403a3fa
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-12-03 16:09:35 +0100

    encore plus mieux

--- exo 1/script.js
+++ exo 1/script.js
@@ -1,8 +1,9 @@
 let add = document.getElementById("add");
 
-add.addEventListener("click", function (){
-    let value =document.getElementById("choix").value;
+add.addEventListener("click", () =>{
+    let value = document.getElementById("choix").value;
     let li = document.createElement("li");
     li.innerHTML = value;
     document.getElementById("liste-commissions").append(li);
+    document.getElementById("choix").value = "";
 })
--- exo 2/script.js
+++ exo 2/script.js
@@ -1,14 +1,15 @@
 let add = document.getElementById("add");
 let del = document.getElementById("del");
 
-add.addEventListener("click", function (){
+add.addEventListener("click", () =>{
     let value =document.getElementById("choix").value;
     let li = document.createElement("li");
     li.innerHTML = value;
     document.getElementById("liste-commissions").append(li);
+    document.getElementById("choix").value = "";
 })
 
-del.addEventListener("click", function (){
+del.addEventListener("click", () =>{
     let li = document.getElementsByTagName("li");
     let nbrLi = li.length;
     if (nbrLi > 0){
