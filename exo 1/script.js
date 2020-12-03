0616aad2957364edfb2d2b800b4ffe5777bab4aa
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-12-03 14:41:39 +0100

    addEventListener

--- exo 1/script.js
+++ exo 1/script.js
@@ -1,6 +1,8 @@
-function addElement(){
+let add = document.getElementById("add");
+
+add.addEventListener("click", function (){
     let value =document.getElementById("choix").value;
     let li = document.createElement("li");
     li.innerHTML = value;
     document.getElementById("liste-commissions").append(li);
-}
+})
--- exo 2/script.js
+++ exo 2/script.js
@@ -1,15 +1,18 @@
-function addElement(){
+let add = document.getElementById("add");
+let del = document.getElementById("del");
+
+add.addEventListener("click", function (){
     let value =document.getElementById("choix").value;
     let li = document.createElement("li");
     li.innerHTML = value;
     document.getElementById("liste-commissions").append(li);
-}
+})
 
-function deleteElement(){
+del.addEventListener("click", function (){
     let li = document.getElementsByTagName("li");
     let nbrLi = li.length;
     if (nbrLi > 0){
         let lastLi = li[nbrLi-1];
         lastLi.innerHTML = lastLi.remove();
     }
-}
+})
